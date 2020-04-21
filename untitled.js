a58aa8f38ab80ec8b5be8777340dc9ff2d123a57
Akash Jeganath <akashjoffl@icloud.com>
2020-04-21 23:05:37 +0530

    18. slice()

--- untitled.js
+++ untitled.js
@@ -1,6 +1,10 @@
-let example = 'Hello World Hello';
+let example = 'Hello World';
+let example2 = example.slice(0,4);
 
-console.log(example.search(/world/gi));
+console.log(example2);
+console.log(example);
+
+
 
 // g -> global
 // if doesn't exist -> -1 
