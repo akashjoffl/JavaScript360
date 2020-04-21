23147a69a4d7085ee671262d451f15dee021c789
Akash Jeganath <akashjoffl@icloud.com>
2020-04-21 23:28:32 +0530

    29. isSafeInteger()

--- untitled.js
+++ untitled.js
@@ -1,6 +1,8 @@
 let example1 = 1.11;
 
-console.log(Number.isNaN(NaN))
-console.log(Number.isNaN('Hello'))
-console.log(Number.isNaN(0))
-// NaN -> Not a Number
+console.log(Number.isSafeInteger(1));
+console.log(Number.isSafeInteger(Infinity));
+console.log(Number.isSafeInteger(Math.pow(2, 25)));
+console.log(Number.isSafeInteger(example1));
+
+// check if it's a integer
