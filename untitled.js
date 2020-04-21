065c602f8350bd83e9750f71c5b9a487f22c317d
Akash Jeganath <akashjoffl@icloud.com>
2020-04-21 23:26:55 +0530

    28. isNaN()

--- untitled.js
+++ untitled.js
@@ -1,6 +1,6 @@
 let example1 = 1.11;
 
-console.log(Number.isInteger(example1));
-
-
-// checking whole numbers true || false boolean
+console.log(Number.isNaN(NaN))
+console.log(Number.isNaN('Hello'))
+console.log(Number.isNaN(0))
+// NaN -> Not a Number
