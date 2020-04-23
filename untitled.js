09c04310b3bde0da67cc9a40344288e0e29c8db1
Akash Jeganath <akashjoffl@icloud.com>
2020-04-24 03:25:38 +0530

    50. pop()

--- untitled.js
+++ untitled.js
@@ -1,21 +1,12 @@
 let example1 = [1, 2, 3, 4, 5, 5, 6, 5];
+let example2 = example1.pop();
 
- let example2 = example1.map((element) => {
-     if (element < 3){
-         return element * 5;
-     }
-     return element + 2; 
-})
-
-// let example2 = [];
-// example1.forEach((element) => {
-//     example2.push(element + 2);
-// })
-
+console.log(example1);
 console.log(example2);
 
-// helps to create new array based off an existing old array    
-// 49. map()
+// to remove the last element of an array
+// 50. pop()
+
 
 
 
