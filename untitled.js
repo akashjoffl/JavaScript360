ac5c2039d6a9937819311320efa3e368afad1088
Akash Jeganath <akashjoffl@icloud.com>
2020-04-24 02:46:25 +0530

    36. concat()

--- untitled.js
+++ untitled.js
@@ -1,12 +1,7 @@
-let example1 = [1, 2, 5, true, 'Hello', {hi: true}];
+let example1 = [1, 2, 5];
+let example2 = [4, 5, 6, [1, 2, 3]];
+let example3 = [true, false]
+let result = example1.concat(example2, example3); 
 
-console.log(
-    typeof example1
-    );
-
-console.log(
-    Array.isArray({})
-);
-// 
-
-// 35. Array Properties
+console.log(result);
+// 36. concat()
