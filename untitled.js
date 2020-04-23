5971e5a481b4736164675cda1ecd5b682868cdbb
Akash Jeganath <akashjoffl@icloud.com>
2020-04-24 03:03:38 +0530

    41. filter()

--- untitled.js
+++ untitled.js
@@ -1,10 +1,15 @@
-let example1 = [1, 2, 3, 4, 5];
+let example2 = [1, 2, 3, 4, 5];
+let example1 = example2.filter((element) => {
+    return element < 4;
+});
 
-let result = example1.fill(1, 1, 4);
-
-console.log(result);
 
 
-// fill method that allows to fill the entire length of an array 
-// overwrite the value of an array with a certain value
-// 40. fill()
+// let result = 
+
+console.log(example1);
+
+
+// takes in callback function and filters out the elements don't pass
+// and filters it into new array
+// 41. filter()
