d591e2cb7473a08ff516cd5ad99dc1ce706611ad
Akash Jeganath <akashjoffl@icloud.com>
2020-04-24 03:30:20 +0530

    52. reduce()

--- untitled.js
+++ untitled.js
@@ -1,11 +1,15 @@
-let example1 = [1, 2, 3, 4, 5, 5, 6, 5];
-let example2 = example1.push(11, 33, 66);
+let example1 = [1, 2, 3, 4, 5, 5, 6, 5].reduce(
+    (currentTotal, currentValue) => {
+        console.log(currentTotal);
+    
+    return currentTotal + currentValue
+    });
+    console.log(example1);
 
-console.log(example1);
-console.log(example2);
+// adding the bunch of value, takes in two required parameters in callback
+// takes in total and add into current
+// 52. reduce()
 
-// adding value at the end of value
-// 51. push()
 
 
 
