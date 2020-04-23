1a142e228e3c4bec2ac4d5ed362666ad88c2e527
Akash Jeganath <akashjoffl@icloud.com>
2020-04-24 03:23:50 +0530

    49. map()

--- untitled.js
+++ untitled.js
@@ -1,12 +1,21 @@
 let example1 = [1, 2, 3, 4, 5, 5, 6, 5];
 
-console.log(
-    example1.lastIndexOf((5))
-);
+ let example2 = example1.map((element) => {
+     if (element < 3){
+         return element * 5;
+     }
+     return element + 2; 
+})
 
+// let example2 = [];
+// example1.forEach((element) => {
+//     example2.push(element + 2);
+// })
 
+console.log(example2);
 
-// finding the first index from last
-// 48. lastIndexOf()
+// helps to create new array based off an existing old array    
+// 49. map()
+
 
 
