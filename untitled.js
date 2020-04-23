a2f9e007c9109e857631091100e4945078da8797
Akash Jeganath <akashjoffl@icloud.com>
2020-04-24 03:32:49 +0530

    54. slice()

--- untitled.js
+++ untitled.js
@@ -1,11 +1,12 @@
 let example1 = [1, 2, 3, 4, 5, 5, 6, 5];
 
-let example2 = example1.shift();
+let example2 = example1.slice(2, 4);
 
-console.log(example1);
+console.log(example2);
 
-// removing the first value of the array
-// 53. shift()
+// copying certain elements in an array
+// 54. slice()
+
 
 
 
