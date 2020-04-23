7ac102e466274986b132b85aee80ad12b6dbd9be
Akash Jeganath <akashjoffl@icloud.com>
2020-04-24 03:40:28 +0530

    58. unshift()

--- untitled.js
+++ untitled.js
@@ -1,9 +1,11 @@
 let example1 = [1, 10, 2, 3, 4, 5, 5, 6, 5, 10];
 
-console.log(example1.toString());
+example1.unshift(0, 55, 66);
+console.log(example1);
 
-// convert the array into strings seperated by commas 
-// 57. toString()
+// adding the element to the front of array
+// 58. unshift()
+
 
 
 
