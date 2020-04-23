264976ba3985585936e2c45da9b6aa0cb9c27d2a
Akash Jeganath <akashjoffl@icloud.com>
2020-04-24 03:17:19 +0530

    47. join()

--- untitled.js
+++ untitled.js
@@ -1,10 +1,11 @@
 let example1 = [1, 2, 3, 4, 5];
 
 console.log(
-    example1.indexOf(2)
+    example1.join('Hello')
 );
 
 
 
-// just to check if the index is in there && there's only one return here
-// 46. indexOf()
+// joining the value and making a one long string
+// 47. join()
+
