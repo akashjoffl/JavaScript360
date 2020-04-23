fa3e2cb2f6140ce424065865eae9343c142e9724
Akash Jeganath <akashjoffl@icloud.com>
2020-04-24 03:13:37 +0530

    45. includes()

--- untitled.js
+++ untitled.js
@@ -1,14 +1,10 @@
 let example1 = [1, 2, 3, 4, 5];
 
-/* for (let i = 0; i < example1.length; i++) {
-    console.log(example1[i]) 
-} */
+console.log(
+    example1.includes(2, 3)
+);
 
-example1.forEach((element, index, array) => {
-    console.log(element);
-    console.log(index); // this is optional 
-    console.log(array); // this is optional 
-})
 
-// callback function iterate through entire array
-// 44. forEach()
+
+// finding out if the array has a particular value
+// 45. includes()
