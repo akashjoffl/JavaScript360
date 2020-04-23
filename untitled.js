6cec1512b578a176ec45466063b681a13421b1fc
Akash Jeganath <akashjoffl@icloud.com>
2020-04-24 03:06:00 +0530

    42. find()

--- untitled.js
+++ untitled.js
@@ -1,15 +1,12 @@
-let example2 = [1, 2, 3, 4, 5];
-let example1 = example2.filter((element) => {
-    return element < 4;
-});
-
+let example1 = [1, 2, 3, 4, 5].find((element) => {
+    return element > 4;
+})
 
 
 // let result = 
 
-console.log(example1);
+console.log(Boolean(example1));
 
 
-// takes in callback function and filters out the elements don't pass
-// and filters it into new array
-// 41. filter()
+// callback function
+// 42. find()
