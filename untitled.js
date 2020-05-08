be433dc45ad92df3a4e9038c1a1d57fb0b348482
Akash Jeganath <akashjoffl@icloud.com>
2020-05-08 19:15:42 +0530

    65. Assignment Operators

--- untitled.js
+++ untitled.js
@@ -1,9 +1,8 @@
 function ternaryOperator(a, b) {
-    let example = 5;
-    return --example;
+    let example = 5 
+    example **= 2; // example = example + 5
+    return example;
 }
 
 let result = ternaryOperator(3, 10);
 console.log(result);
-
-// preFix and postFix
