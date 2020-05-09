6f0ae14189d5d4eb5b0fb017bdbbe578b7884231
Akash Jeganath <akashjoffl@icloud.com>
2020-05-09 09:52:02 +0530

    66. Objects Intro

--- untitled.js
+++
@@ -1,8 +0,0 @@
-function ternaryOperator(a, b) {
-    let example = 5 
-    example **= 2; // example = example + 5
-    return example;
-}
-
-let result = ternaryOperator(3, 10);
-console.log(result);
