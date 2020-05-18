812ed666850bc19e0d079f32b9853ff41cf5ff3d
Akash Jeganath <akashjoffl@icloud.com>
2020-05-18 11:09:12 +0530

    109. localStorage.setItem() & localStorage.getItem()

--- index.js
+++
@@ -1,9 +0,0 @@
-let exampleJSON = JSON.stringify({example: true, name: "Akash", id: 123});
-// Object to String
-
-console.log(typeof exampleJSON);
-console.log(exampleJSON);
-
-exampleJSON = JSON.parse(exampleJSON); // String to Object
-console.log(typeof exampleJSON);
-console.log(exampleJSON);
