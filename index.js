2db825d54aeb86bfe75050f9b7403ecf5456cd18
Akash Jeganath <akashjoffl@icloud.com>
2020-05-19 21:47:40 +0530

    114. For ...of Loop

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
-let exampleObject = {
-    first: "Akash",
-    last: "Jeganath",
-    codingStatus: "God Mode"
-};
+const exampleArray = ["Hello", "Bye", "Hi", "Yolo"];
 
-//const {first, last, codingStatus} = exampleObject;
+for (const word of exampleArray){
+    console.log(word);
+}
 
-//console.log(first, codingStatus);
-// destructure Objects
-
-
-let exampleArray = [1, 2, 3, 4, 5];
-const [first, second, third, forth, fifth] = exampleArray;
-
-console.log(fifth);
-
-//destructing Array
+// of -> value
+// in -> index
