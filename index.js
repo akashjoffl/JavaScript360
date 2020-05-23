1a877744554d39e4b29a378d854b5438e73218f3
Akash Jeganath <akashjoffl@icloud.com>
2020-05-23 19:24:19 +0530

    124. Closure

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-function add() {
-    let array = [];
-    for (let i = 0; i < arguments.length; i++) {
-        array.push(arguments[i]);
-    }
-    console.log(array);
-}
+let addEx = (function() {
+    let total = 0;
 
-add(3, 555, 69); 
+    return function (param1) {
+        return total += param1;
+    }
+})();
+
+console.log(addEx(5));
+console.log(addEx(10));
+console.log(addEx(5));
