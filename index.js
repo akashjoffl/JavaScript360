02e6425166deb3e8e9d7834d40c422cde572f3d9
Akash Jeganath <akashjoffl@icloud.com>
2020-05-24 14:42:25 +0530

    126. Math.random()

--- index.js
+++ index.js
@@ -1,17 +1,1 @@
-console.log(Math.PI);
-
-console.log(Math.round(24.92));
-
-console.log(Math.pow(10, 2));
-
-console.log(Math.sqrt(100));
-
-console.log(Math.abs(-100));
-
-console.log(Math.ceil(10.11));
-
-console.log(Math.floor(19.233));
-
-console.log(Math.min(10, 01, 0.2, 100));
-
-console.log(Math.max(10, 01, 0.2, 100));
+console.log(Math.floor(Math.random()* 10) +10 );
