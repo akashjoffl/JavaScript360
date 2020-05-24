feeeeed0735c5f0fb617b02420fb5ec9dd307730
Akash Jeganath <akashjoffl@icloud.com>
2020-05-24 14:35:42 +0530

    125. Math Introduction

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-let addEx = (function() {
-    let total = 0;
+console.log(Math.PI);
 
-    return function (param1) {
-        return total += param1;
-    }
-})();
+console.log(Math.round(24.92));
 
-console.log(addEx(5));
-console.log(addEx(10));
-console.log(addEx(5));
+console.log(Math.pow(10, 2));
+
+console.log(Math.sqrt(100));
+
+console.log(Math.abs(-100));
+
+console.log(Math.ceil(10.11));
+
+console.log(Math.floor(19.233));
+
+console.log(Math.min(10, 01, 0.2, 100));
+
+console.log(Math.max(10, 01, 0.2, 100));
